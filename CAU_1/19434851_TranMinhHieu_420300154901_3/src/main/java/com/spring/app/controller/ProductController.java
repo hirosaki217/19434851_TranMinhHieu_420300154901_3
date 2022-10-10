d2fcf0f7edfd847be5adec6c6617eac6d79619f8
hirosaki217 <hirosaki217@gmail.com>
2022-10-10 12:52:39 +0700

    finish request product

--- CAU_1/19434851_TranMinhHieu_420300154901_3/src/main/java/com/spring/app/controller/ProductController.java
+++ CAU_1/19434851_TranMinhHieu_420300154901_3/src/main/java/com/spring/app/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.spring.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,10 @@ public class ProductController {
 		Product p = productService.createProduct(product);
 		return ResponseEntity.ok(p);
 		
+	}
+	
+	@GetMapping("/getProducts")
+	public ResponseEntity<List<Product>> getProducts() {
+		return  ResponseEntity.ok(productService.getProducts());
 	}
 }
--- CAU_1/19434851_TranMinhHieu_420300154901_3/src/main/java/com/spring/app/service/ProductServiceImpl.java
+++ CAU_1/19434851_TranMinhHieu_420300154901_3/src/main/java/com/spring/app/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.spring.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,11 @@ public class ProductServiceImpl implements ProductService{
 	public Product createProduct(Product product) {
 		
 		return productRepository.saveAndFlush(product);
+	}
+
+	public List<Product> getProducts() {
+		
+		return productRepository.findAll();
 	}
 	
 }
